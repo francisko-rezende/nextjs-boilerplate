36ca52d33b89222e07ec8b42522895119fbe4b71
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-02-15 18:07:57 -0300

    feat(esling): create initial version of eslint config with sorter plugin + nextjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +14,15 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   eslintConfigPrettier,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
